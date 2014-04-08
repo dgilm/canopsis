6992ffdc3a320a9f7aac56997621060269aee8ef
eregnie@capensis.fr <eregnier@capensis.fr>
2014-04-08 16:23:40 +0200

    added event navigation

---
+++ sources/webcore/var/www/canopsis/core/controller/eventnavigation.js
@@ -0,0 +1,64 @@
+/*
+# Copyright (c) 2014 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+define([
+	'jquery',
+	'app/lib/ember',
+	'app/application',
+	'app/model/eventnavigation'
+], function($, Ember, Application, Eventnavigation) {
+	Application.EventnavigationRoute = Application.CrecordsRoute.extend({
+		setupController: function(controller, model) {
+			controller.set('content', model);
+			controller.set('toolitems', controller.toolbar);
+		},
+
+		model: function() {
+			return this.store.findAll('eventnavigation');
+		}
+	});
+
+	Application.EventnavigationController = Application.CrecordsController.extend({
+		toolbar: [{
+			title: 'Refresh',
+			action: 'refresh',
+			icon: 'refresh'
+		},{
+			title: 'Add',
+			action: 'showAddForm',
+			icon: 'plus-sign'
+		},{
+			title: 'Duplicate',
+			action: 'duplicate',
+			icon: 'file'
+		},{
+			title: 'Remove',
+			action: 'remove',
+			icon: 'trash'
+		}],
+
+		actions: {
+			refresh: function() {
+				controller.set('content', this.store.findAll('event'));
+			}
+		}
+	});
+
+	return Application.EventnavigationController;
+});
---
+++ sources/webcore/var/www/canopsis/core/model/eventnavigation.js
@@ -0,0 +1,71 @@
+/*
+# Copyright (c) 2014 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+define([
+	'jquery',
+	'app/lib/ember',
+	'app/lib/ember-data',
+	'app/application',
+	'app/model/crecord'
+], function($, Ember, DS, Application, Crecord) {
+	Application.Eventnavigation = Application.Crecord.extend({
+		timestamp	: DS.attr('number'),
+		component	: DS.attr('string'),
+		resource	: DS.attr('string'),
+		connector	: DS.attr('string'),
+		output		: DS.attr('string'),
+		state		: DS.attr('number')
+	});
+
+	Application.Eventnavigation.reopenClass({
+		findAll: function(store, authkey) {
+			return $.ajax({
+				url: '/rest/events/event',
+				method: 'GET',
+				contentType: 'application/json',
+				data: {
+					authkey: authkey
+				}
+			});
+		},
+
+		extractFindAll: function(store, payload) {
+			var events = [];
+
+			for(var i = 0; i < payload.data.length; i++) {
+				var evt = payload.data[i],
+					key;
+
+/*
+				for(key in evt) {
+					if (!evt[key]){
+						console.log(key)
+						delete evt[key];
+					}
+				}
+*/
+				events.push(evt);
+			}
+
+			return events;
+		}
+	});
+
+	return Application.Eventnavigation;
+});
