435a1073c4c8dbbc94307ef57da7f20f0204b158
Gwenaël Pluchon <gpluchon@capensis.fr>
2014-04-30 11:01:19 +0200

    clean a bit console API to remove useless function and provide clear autocompletion

--- sources/webcore/var/www/canopsis/core/lib/console.js
+++ sources/webcore/var/www/canopsis/core/lib/console.js
@@ -1,4 +1,9 @@
 define(['consolejs'], function(Ember) {
+    delete console.__proto__['constructor'];
+    delete console.__proto__['isPrototypeOf'];
+    delete console.__proto__['toLocaleString'];
+    delete console.__proto__['valueOf'];
+    delete console['init'];
 
 	return console;
 });
