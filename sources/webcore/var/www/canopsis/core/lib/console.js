0bf0af328fc7d140b54c4458d1773e25e7846be6
Gwenaël Pluchon <gpluchon@capensis.fr>
2014-04-30 11:27:57 +0200

    fix log helper

--- sources/webcore/var/www/canopsis/core/lib/console.js
+++ sources/webcore/var/www/canopsis/core/lib/console.js
@@ -1,8 +1,5 @@
 define(['consolejs'], function(Ember) {
-    delete console.__proto__['constructor'];
-    delete console.__proto__['isPrototypeOf'];
-    delete console.__proto__['toLocaleString'];
-    delete console.__proto__['valueOf'];
+
     delete console['init'];
 
 	return console;
