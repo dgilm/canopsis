c911d38752905013a7239e11a9e58ec11c91f964
Gwenaël Pluchon <gpluchon@capensis.fr>
2014-05-06 17:22:16 +0200

    remove js files loading from main.js

--- sources/webcore/var/www/canopsis/main.js
+++ sources/webcore/var/www/canopsis/main.js
@@ -1,35 +1,9 @@
-var routes;
-
-require([
-    'plugins',
-    'text!canopsis/enabled.json',
-    'text!canopsis/core/files/manifest.json',
-    'text!canopsis/core/files/routes.json',
-    'text!canopsis/core/files/files.json'
-], function() {
-    var plugins = [];
-    var path = "canopsis/";
-    var files;
-
-    try {
-	plugins = Plugins.getPlugins(path);
-	plugins = Plugins.resolveDependancies(plugins);
-    } catch (e) {
-	console.log("PluginError: " + e);
-    }
-
-    routes = Manifest.fetchRoutes(plugins, path);
-    files = Manifest.fetchFiles(plugins, path);
-    files = files.map(function(e, i, a) { return (e.replace("canopsis/core/", "app/")); });
-    require(files);
-});
-
-
 define([
     'canopsis/commit',
     'jquery',
     'seeds/RoutesLoader',
     'app/application',
+    'app/controller/application',
     'app/routes/authenticated',
     'app/routes/paginated',
     'app/adapters/application',
